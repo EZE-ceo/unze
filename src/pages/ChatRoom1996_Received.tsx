a9f0c72b779a748dc02260cb9dfac1f4a668d5bc
E:ZE <gagmagazine@EZE-2.local>
2025-05-22 09:27:02 +0900

    Fix: 이미지 경로 절대경로(/src/)로 수정

--- src/pages/ChatRoom1996_Received.tsx
+++ src/pages/ChatRoom1996_Received.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../style/ChatRoom1996_Received.css';
-import beepImage from '../assets/beep.png';
+import beepImage from '/src/assets/beep.png';
 
 const ChatRoom1996_Received = () => {
   const navigate = useNavigate();
