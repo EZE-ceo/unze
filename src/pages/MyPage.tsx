8823cd0c97c2b4510fdc1c10bfd207f743a73ca0
E:ZE <gagmagazine@EZE-2.local>
2025-05-22 10:24:24 +0900

    Fix: 이미지 경로 대소문자 수정 (edit_Icon.png)

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
-import editIcon from '../assets/edit_icon.png';
+import editIcon from "../assets/edit_Icon.png";
 import logoImg from '../assets/unze_full_logo.png';
 import logoutImg from '../assets/logout.png';
 
